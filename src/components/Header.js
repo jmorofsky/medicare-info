6e37240c9c3d2f5512511cfef9309121b7b97803
dra2a <63828961+dra2a@users.noreply.github.com>
2021-08-17 22:45:52 -0400

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import '../css/Header.css'
+
+function Header() {
+    return (
+        <div className='header'>
+            <h1 className='title'>STUCK ON MEDICARE?</h1>
+            <h2 className='subtitle'>LET'S FIGURE IT OUT TOGETHER</h2>
+
+            <p>
+                I'm Meghan Morofsky, and I have been helping everyday people like you find a plan that fits their
+                needs for the past 3 years. I can help you review all your Medicare options so you can make an
+                informed decision about your coverage.
+            </p>
+        </div>
+    )
+}
+
+export default Header
