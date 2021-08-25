90cc5530402cecad1c73341c967b2a4c20ce09c0
dra2a <63828961+dra2a@users.noreply.github.com>
2021-08-25 02:12:58 -0400

    added quote section

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -3,8 +3,12 @@ import '../css/Quote.css'
 function Quote() {
     return (
         <div className='quote-bg'>
-            <p style={{color: '#fff'}}>Choose your Medicare plan <u>with the rest of your life in mind.</u></p>
-            <p>- Steve Vernon, author, <em>Retirement Game-Changers</em></p>
+            <p style=
+                {{ color: '#fff', textAlign: 'center', fontSize: '24px', paddingTop: '50px' }}>
+                "Choose your Medicare plan <u>with the rest of your life in mind.</u>"</p>
+            <p style=
+                {{ color: '#fff', textAlign: 'center', fontSize: '16px', paddingBottom: '50px' }}>
+                – Steve Vernon, author of <em>Retirement Game-Changers</em></p>
         </div>
     )
 }
